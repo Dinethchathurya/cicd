1a28896aabf42d0ba1b7d9832a8f7a148e11be76
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2025-01-02 20:46:13 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow external access to the development server
+    port: 5173,       // Ensure it's the correct port you're exposing
+  }
 })
